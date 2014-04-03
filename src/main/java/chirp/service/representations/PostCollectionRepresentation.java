8806def9fb76a47d2a9bd18309e004498c97736d
Vinod Damle <vdamle@cisco.com>
2014-04-03 11:07:55 -0700

    - additional update to data presentation

--- src/main/java/chirp/service/representations/PostCollectionRepresentation.java
+++ src/main/java/chirp/service/representations/PostCollectionRepresentation.java
@@ -9,29 +9,39 @@ import javax.ws.rs.core.UriBuilder;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
+import org.codehaus.jackson.annotate.JsonCreator;
+import org.codehaus.jackson.annotate.JsonProperty;
+
 import chirp.model.Post;
 import chirp.model.User;
 
 @XmlRootElement
 public class PostCollectionRepresentation {
 
-	private Collection<PostRepresentation> postReps = new ArrayList<>();
+	private Collection<PostRepresentation> posts = new ArrayList<>();
 	private URI self;
 	
 	public PostCollectionRepresentation() {
 		
 	}
 	
-	public PostCollectionRepresentation(Collection<Post> posts, User user) {
-		for(Post post : posts) {
-			postReps.add(new PostRepresentation(post, true));
+	public PostCollectionRepresentation(Collection<Post> postList, User user) {
+		for(Post post : postList) {
+			posts.add(new PostRepresentation(post, true));
 		}
 		self  = UriBuilder.fromPath("/post").path(user.getUsername()).build();
 	}
 	
+	@JsonCreator
+	public PostCollectionRepresentation(@JsonProperty("self") URI self,
+										@JsonProperty("posts")Collection<PostRepresentation> postList) {
+		this.self = self;
+		this.posts = postList;
+	}
+	
 	@XmlElement
-	public Collection<PostRepresentation> getPost() {
-		return Collections.unmodifiableCollection(postReps);
+	public Collection<PostRepresentation> getPosts() {
+		return Collections.unmodifiableCollection(posts);
 	}
 	
 	@XmlElement
--- src/main/java/chirp/service/representations/UserCollectionRepresentation.java
+++ src/main/java/chirp/service/representations/UserCollectionRepresentation.java
@@ -9,30 +9,40 @@ import javax.ws.rs.core.UriBuilder;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
+import org.codehaus.jackson.annotate.JsonCreator;
+import org.codehaus.jackson.annotate.JsonProperty;
+
 import chirp.model.User;
 
 @XmlRootElement
 public class UserCollectionRepresentation {
-	
-	private Collection<UserRepresentation> userReps = new ArrayList<>();
+
+	private Collection<UserRepresentation> users = new ArrayList<>();
 	private URI self;
-	
+
 	public UserCollectionRepresentation() {
-		
+
 	}
 
-	public UserCollectionRepresentation(Collection<User> users) {
-		for (User user : users) {
-			userReps.add(new UserRepresentation(user, true));
+	public UserCollectionRepresentation(Collection<User> userList) {
+		for (User user : userList) {
+			users.add(new UserRepresentation(user, true));
 		}
 		self = UriBuilder.fromPath("/user").build();
 	}
 	
-	@XmlElement
-	public Collection<UserRepresentation> getUser() {
-		return Collections.unmodifiableCollection(userReps);
+	@JsonCreator
+	public UserCollectionRepresentation(@JsonProperty("self") URI self,
+										@JsonProperty("users")Collection<UserRepresentation> users) {
+		this.self = self;
+		this.users = users;
 	}
-	
+
+	@XmlElement
+	public Collection<UserRepresentation> getUsers() {
+		return Collections.unmodifiableCollection(users);
+	}
+
 	@XmlElement
 	public URI getSelf() {
 		return self;
