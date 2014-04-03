baaadece5d87b58bd894e02ff3deff1470fae9ea
Vinod Damle <vdamle@cisco.com>
2014-04-03 07:54:40 -0700

    - Add a link to self in user/post representation

--- src/main/java/chirp/service/representations/PostRepresentation.java
+++ src/main/java/chirp/service/representations/PostRepresentation.java
@@ -1,5 +1,8 @@
 package chirp.service.representations;
 
+import java.net.URI;
+
+import javax.ws.rs.core.UriBuilder;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
@@ -15,17 +18,21 @@ public class PostRepresentation {
 	
 	private String content;
 	private String timestamp;
+	private URI self;
 	
 	public PostRepresentation() { /* For xml representation */ }
 	
 	@JsonCreator
-	public PostRepresentation(@JsonProperty("content") String content,
+	public PostRepresentation(@JsonProperty("self") URI self,
+							  @JsonProperty("content") String content,
 							  @JsonProperty("timestamp") String timestamp) {
+		this.self = self;
 		this.content = content;
 		this.timestamp = timestamp;
 	}
 	
 	public PostRepresentation(Post post) {
+		this.self = UriBuilder.fromPath("post").path(post.getUser().getUsername()).path(post.getTimestamp().toString()).build();
 		this.content = post.getContent();
 		this.timestamp = post.getTimestamp().toString();
 	}
@@ -42,5 +49,10 @@ public class PostRepresentation {
 	@XmlElement
 	public String getTimestamp() {
 		return timestamp;
+	}
+	
+	@XmlElement
+	public URI getSelf() {
+		return self;
 	}
 }
--- src/main/java/chirp/service/representations/UserRepresentation.java
+++ src/main/java/chirp/service/representations/UserRepresentation.java
@@ -1,5 +1,8 @@
 package chirp.service.representations;
 
+import java.net.URI;
+
+import javax.ws.rs.core.UriBuilder;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
@@ -13,17 +16,21 @@ public class UserRepresentation {
 	
 	private String username;
 	private String realname;
+	private URI self;
 	
 	public UserRepresentation() { /* required for xml representation */ }
 	
 	@JsonCreator
-	public UserRepresentation(@JsonProperty("username") String username,
+	public UserRepresentation(@JsonProperty("self") URI self,
+							  @JsonProperty("username") String username,
 							  @JsonProperty("realname")String realname) {
+		this.self = self;
 		this.username = username;
 		this.realname = realname;
 	}
 	
 	public UserRepresentation(User user) {
+		this.self = UriBuilder.fromPath("user").path(user.getUsername()).build();
 		this.username = user.getUsername();
 		this.realname = user.getRealname();
 	}
@@ -44,5 +51,14 @@ public class UserRepresentation {
 	
 	public void setRealname(String realname) {
 		this.realname = realname;
+	}
+
+	@XmlElement
+	public URI getSelf() {
+		return self;
+	}
+
+	public void setSelf(URI self) {
+		this.self = self;
 	}
 }
